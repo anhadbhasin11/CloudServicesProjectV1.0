9ceb11c5eee07b1ddc0917e3d134eb6ddb348858
anhadbhasin11 <anhad_bhasin@yahoo.co.in>
2016-05-12 19:27:10 -0700

    commit3

--- server/app.js
+++ server/app.js
@@ -7,6 +7,16 @@ var aws = require('./aws');
 
 var app = express();
 
+var passport = require('passport');
+
+// This is the file we created in step 2.
+// This will configure Passport to use Auth0
+var strategy = require('./setup-passport');
+
+// Session and cookies middlewares to keep user logged in
+var cookieParser = require('cookie-parser');
+var session = require('express-session');
+
 app.use(express.static(path.join(__dirname, '../')));
 
 routes.setup(app, aws);
