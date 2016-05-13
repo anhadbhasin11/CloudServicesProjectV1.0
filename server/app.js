39edca379b4861357a7f3b77ad3593b6c134dcca
anhadbhasin11 <anhad_bhasin@yahoo.co.in>
2016-05-12 19:34:22 -0700

    commit5

--- server/app.js
+++ server/app.js
@@ -11,7 +11,7 @@ var passport = require('passport');
 
 // This is the file we created in step 2.
 // This will configure Passport to use Auth0
-var strategy = require('setup-passport');
+var strategy = require('setup-passport.js');
 
 // Session and cookies middlewares to keep user logged in
 var cookieParser = require('cookie-parser');
